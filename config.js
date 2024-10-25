1be94dea85c881f04b3168bce61df3b128d887fe
VandalRGB <141866645+VandalRGB369@users.noreply.github.com>
2024-10-25 03:40:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
 process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_25f538740487024c8bbbe21cf007fa737f').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
